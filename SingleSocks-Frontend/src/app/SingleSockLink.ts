2ae57e1df36517a7b1b0192d702aad087aa0b5e5
JamieWindfox <jamie.windfuchs@gmail.com>
2022-05-25 19:37:46 +0200

    Made links in html home site loading dynamically

--- SingleSocks-Frontend/src/app/SingleSockLink.ts
+++ SingleSocks-Frontend/src/app/SingleSockLink.ts
@@ -2,5 +2,37 @@ export class SingleSockLink {
   link: string;
   name: string;
 
+  constructor(link: string, name: string) {
+    this.link = link;
+    this.name = name;
+  }
+}
 
+export class SingleSockLinkList {
+  links: SingleSockLink[];
+
+  constructor() {
+    this.links = [
+      new SingleSockLink("home.component.html", "Home"),
+      new SingleSockLink("", "Find Socks"),
+      new SingleSockLink("", "Give Away Socks"),
+      new SingleSockLink("", "Find Donation Boxes"),
+      new SingleSockLink("", "DIY Corner"),
+      new SingleSockLink("", "About"),
+      new SingleSockLink("", "Fun")
+    ];
+  }
+}
+
+export class AuthLinkList {
+  links: SingleSockLink[];
+
+  constructor() {
+    this.links = [
+      new SingleSockLink("", "Login"),
+      new SingleSockLink("", "Register"),
+      new SingleSockLink("", "Profile"),
+      new SingleSockLink("", "Logout")
+    ]
+  }
 }
--- SingleSocks-Frontend/src/app/home/home.component.ts
+++ SingleSocks-Frontend/src/app/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {AuthLinkList, SingleSockLinkList} from "../SingleSockLink";
 
 @Component({
   selector: 'app-home',
@@ -7,11 +8,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  links: string[] = ["Home", "About"];
+  linkList: SingleSockLinkList;
+  authLinkList: AuthLinkList;
 
   constructor() { }
 
   ngOnInit(): void {
+    this.linkList = new SingleSockLinkList();
+    this.authLinkList = new AuthLinkList();
   }
 
 }
