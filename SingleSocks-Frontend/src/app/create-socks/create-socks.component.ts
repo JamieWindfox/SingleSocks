19e23cf1ba5e147d1c73e916596f3f75f73eff84
JamieWindfox <jamie.windfuchs@gmail.com>
2022-05-26 14:28:53 +0200

    adapted create socks page to angular

--- SingleSocks-Frontend/src/app/create-socks/create-socks.component.ts
+++ SingleSocks-Frontend/src/app/create-socks/create-socks.component.ts
@@ -3,6 +3,8 @@ import {AuthLinkList, SingleSockLinkList} from "../SingleSockLink";
 import {Color} from "../Color";
 import {SockSize} from "../SockSize";
 import {SockPattern} from "../SockPattern";
+import {SockType} from "../SockType";
+import {Material} from "../Material";
 
 @Component({
   selector: 'app-create-socks',
@@ -16,6 +18,9 @@ export class CreateSocksComponent implements OnInit {
   colors: string[];
   sizes: string[];
   patterns: string[];
+  types: string[];
+  materials: string[];
+
 
   constructor() { }
 
@@ -27,10 +32,16 @@ export class CreateSocksComponent implements OnInit {
     });
     this.sizes = Object.values(SockSize).filter((item) => {
       return isNaN(Number(item));
-    })
+    });
     this.patterns = Object.values(SockPattern).filter((item) => {
       return isNaN(Number(item));
-    })
+    });
+    this.types = Object.values(SockType).filter((item) => {
+      return isNaN(Number(item));
+    });
+    this.materials = Object.values(Material).filter((item) => {
+      return isNaN(Number(item));
+    });
   }
 
 }
