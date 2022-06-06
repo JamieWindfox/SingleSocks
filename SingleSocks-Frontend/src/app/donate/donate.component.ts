4627372d2e79997312d996e5c69a833dcca94298
Kuuko <miguelkristopharcilla@gmail.com>
2022-06-06 10:10:15 +0200

    Added mock popup for donation box markers

--- SingleSocks-Frontend/src/app/donate/donate.component.ts
+++ SingleSocks-Frontend/src/app/donate/donate.component.ts
@@ -28,7 +28,7 @@ export class DonateComponent implements OnInit {
     }).addTo(map);
 
     for (let coords of this.boxes) {
-      L.marker(coords as LatLngExpression).addTo(map)
+      L.marker(coords as LatLngExpression).addTo(map).bindPopup("Teststraße 22, 10XX Vienna <br> 09:00-23:00")
     }
   }
 
