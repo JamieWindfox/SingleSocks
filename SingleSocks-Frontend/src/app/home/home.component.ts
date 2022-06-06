15f1b53f2ded134adb8c7f9b6b443930e59dc9fc
Kuuko <miguelkristopharcilla@gmail.com>
2022-06-06 10:21:48 +0200

    Shortened featured socks

--- SingleSocks-Frontend/src/app/home/home.component.ts
+++ SingleSocks-Frontend/src/app/home/home.component.ts
@@ -1,5 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {AuthLinkList, SingleSockLinkList} from "../SingleSockLink";
+import {SockProfile} from "../sock-profile";
 
 @Component({
   selector: 'app-home',
@@ -11,7 +12,78 @@ export class HomeComponent implements OnInit {
   linkList: SingleSockLinkList;
   authLinkList: AuthLinkList;
 
-  constructor() { }
+  socks: SockProfile[] = [
+    {
+      name: "Grey Pair",
+      picture: "greysocks.png",
+      searchingFor: "a new home",
+      size: 43,
+      condition: "pretty good",
+      manufacturer: "Someone",
+      price: 2,
+      description: "Some Descr",
+      deliveryPrime: 2.5
+    },
+    {
+      name: "Red Single",
+      picture: "redsinglesock.png",
+      searchingFor: "a match",
+      size: 36,
+      condition: "good",
+      manufacturer: "Someone",
+      price: 2,
+      description: "Some Descr",
+      deliveryPrime: 2.5
+    },
+    {
+      name: "Blue Striped",
+      picture: "bluestripesocks.png",
+      searchingFor: "a new home",
+      size: 38,
+      condition: "new",
+      manufacturer: "Someone",
+      price: 2,
+      description: "Some Descr",
+      deliveryPrime: 2.5
+    },
+    {
+      name: "White Adidas Stripe",
+      picture: "whiteadidasstripe.png",
+      searchingFor: "a match",
+      size: 40,
+      condition: "good as new",
+      manufacturer: "Someone",
+      price: 2,
+      description: "Some Descr",
+      deliveryPrime: 2.5
+    },
+    {
+      name: "Model Red Single",
+      picture: "redsinglesock.png",
+      searchingFor: "a match",
+      size: 36,
+      condition: "good",
+      manufacturer: "Someone",
+      price: 2,
+      description: "Some Descr",
+      deliveryPrime: 2.5
+    },
+    {
+      name: "Model Red Single",
+      picture: "redsinglesock.png",
+      searchingFor: "a match",
+      size: 36,
+      condition: "good",
+      manufacturer: "Someone",
+      price: 2,
+      description: "Some Descr",
+      deliveryPrime: 2.5
+    }
+  ]
+
+
+  constructor() {
+  }
 
   ngOnInit(): void {
     this.linkList = new SingleSockLinkList();
---
+++ SingleSocks-Frontend/src/app/sock-profile.ts
@@ -0,0 +1,11 @@
+export interface SockProfile {
+  name: string;
+  searchingFor: string;
+  size: number;
+  condition: string;
+  manufacturer: string;
+  description: string;
+  price: number;
+  deliveryPrime: number;
+  picture: string;
+}
