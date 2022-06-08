d41fc23a7fd84559a2efb16e587ce7102367f70b
JamieWindfox <jamie.windfuchs@gmail.com>
2022-06-08 09:28:55 +0200

    added typescript to diy corner - not functional at the moment!

--- SingleSocks-Frontend/src/app/diy-corner/diy-corner.component.ts
+++ SingleSocks-Frontend/src/app/diy-corner/diy-corner.component.ts
@@ -7,9 +7,37 @@ import {Component, OnInit} from '@angular/core';
 })
 export class DiyCornerComponent implements OnInit {
 
-  constructor() { }
+  constructor() {
+  }
 
   ngOnInit(): void {
   }
 
+}
+
+let slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n : number) {
+  showSlides(slideIndex += n);
+}
+
+function currentSlide(n : number) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n : number) {
+  let i;
+  let slides = document.getElementsByClassName("diySlides");
+  let dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active";
 }
