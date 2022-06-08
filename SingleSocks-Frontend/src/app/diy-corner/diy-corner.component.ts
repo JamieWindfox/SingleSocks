f5b96f3e40033988372c981fc2453139f0be3b7e
JamieWindfox <jamie.windfuchs@gmail.com>
2022-06-08 10:22:06 +0200

    adapted typescript for DIY corner - not functional at the moment!

--- SingleSocks-Frontend/src/app/diy-corner/diy-corner.component.ts
+++ SingleSocks-Frontend/src/app/diy-corner/diy-corner.component.ts
@@ -7,37 +7,67 @@ import {Component, OnInit} from '@angular/core';
 })
 export class DiyCornerComponent implements OnInit {
 
+  slideIndex : number = 1;
+  diyDisplay_1 : string;
+  diyDisplay_2 : string;
+  diyDisplay_3 : string;
+
   constructor() {
+    this.showSlides(this.slideIndex);
   }
 
   ngOnInit(): void {
   }
 
-}
-
-let slideIndex = 1;
-showSlides(slideIndex);
-
-function plusSlides(n : number) {
-  showSlides(slideIndex += n);
-}
-
-function currentSlide(n : number) {
-  showSlides(slideIndex = n);
-}
-
-function showSlides(n : number) {
-  let i;
-  let slides = document.getElementsByClassName("diySlides");
-  let dots = document.getElementsByClassName("dot");
-  if (n > slides.length) {slideIndex = 1}
-  if (n < 1) {slideIndex = slides.length}
-  for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";
+  changeSlide(n : number) {
+    this.showSlides(this.slideIndex += n);
   }
-  for (i = 0; i < dots.length; i++) {
-    dots[i].className = dots[i].className.replace(" active", "");
+
+  currentSlide(n : number) {
+    this.showSlides(this.slideIndex = n);
   }
-  slides[slideIndex-1].style.display = "block";
-  dots[slideIndex-1].className += " active";
+
+  showSlides(n : number) {
+    let i;
+    let slides = document.getElementsByClassName("mySlides");
+    let dots = document.getElementsByClassName("dot");
+    if (n > slides.length) {this.slideIndex = 1}
+    if (n < 1) {this.slideIndex = slides.length}
+    for (i = 0; i < slides.length; i++) {
+        this.triggerSlide(i+1, "none");
+    }
+    for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active", "");
+    }
+    for (i = 0; i < slides.length; i++) {
+      if(slides[this.slideIndex-1].id == ("diy_") + i.toString()) {
+        this.triggerSlide(i+1, "block");
+      }
+    }
+    //slides[this.slideIndex-1].style.display = "block";
+    //dots[this.slideIndex-1].className += " active";
+  }
+
+  triggerSlide(n : number, action : string) {
+    switch(n){
+      case 1: this.diyDisplay_1 = action; break;
+      case 2: this.diyDisplay_2 = action; break;
+      case 3: this.diyDisplay_3 = action; break;
+      default: console.log("Invalid ID of DIY Display");
+    }
+  }
+
+  showSlide(n : number) {
+    switch(n){
+      case 1: this.diyDisplay_1 = "block"; break;
+      case 2: this.diyDisplay_2 = "block"; break;
+      case 3: this.diyDisplay_3 = "block"; break;
+      default: console.log("Invalid ID of DIY Display");
+    }
+  }
+
+
+
+
 }
+
