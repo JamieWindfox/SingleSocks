e562957a8ec37773f41e2d33906a3f8ea5796c53
Miguel Arcilla <miguelkristopharcilla@gmail.com>
2022-06-20 00:21:57 +0200

    Added frontend logic to delete user's sock

--- SingleSocks-Frontend/src/app/services/user.service.ts
+++ SingleSocks-Frontend/src/app/services/user.service.ts
@@ -1,11 +1,17 @@
 import {Injectable} from '@angular/core';
+import {HttpClient} from "@angular/common/http";
+import {UserInfo} from "../user";
 
 @Injectable({
   providedIn: 'root'
 })
 export class UserService {
+  private url = "api/users"
 
-  constructor() {
+  constructor(private httpClient: HttpClient) {
   }
-  // TODO
+
+  getUserInfo() {
+    return this.httpClient.get<UserInfo>(this.url + "/current", {observe: "response"});
+  }
 }
--- SingleSocks-Frontend/src/app/user-profile/user-profile.component.ts
+++ SingleSocks-Frontend/src/app/user-profile/user-profile.component.ts
@@ -1,6 +1,10 @@
 import {Component, OnInit} from '@angular/core';
 import {SockProfile} from "../sock-profile";
 import {SockService} from "../services/sock.service";
+import {environment} from "../../environments/environment";
+import {AuthService} from "../services/auth.service";
+import {UserService} from "../services/user.service";
+import {UserInfo} from "../user";
 
 @Component({
   selector: 'app-user-profile',
@@ -9,26 +13,29 @@ import {SockService} from "../services/sock.service";
 })
 export class UserProfileComponent implements OnInit {
 
-  userInformation = true;
-
   userSocks: SockProfile[] = [];
+  userInfo: UserInfo;
 
-
-  constructor(private sockService: SockService) {
+  constructor(private sockService: SockService, private authService: AuthService, private userService: UserService) {
   }
 
   ngOnInit(): void {
 
-  this.sockService.query().subscribe(result => {
-    this.userSocks = result.body;
+    this.userService.getUserInfo().subscribe(result => {
+      this.userInfo = result.body;
+    })
 
-    for (let sock of this.userSocks) {
-      this.sockService.queryImage(sock._id).subscribe(result => {
-        sock.picture = this.sockService.getImagePath(result.body);
-        });
+    this.sockService.query().subscribe(result => {
+      this.userSocks = result.body.filter(sock => sock.user == this.authService.getUserId());
+      for (let sock of this.userSocks) {
+        sock.picture = environment.serverUrl + 'api/images/' + sock._id;
       }
     });
-
   }
 
+  delete(id: string) {
+    this.sockService.delete(id).subscribe(() => {
+      this.userSocks = this.userSocks.filter(sock => sock._id != id);
+    })
+  }
 }
